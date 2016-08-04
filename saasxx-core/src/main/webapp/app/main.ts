7760d1492b60d239f28fa45f747b794ac50bf0b0
lujijiang <lujijiang@gmail.com>
2016-08-04 17:42:13 +0800

    集成ng2-bootstrap插件

--- saasxx-core/src/main/webapp/app/main.ts
+++ saasxx-core/src/main/webapp/app/main.ts
@@ -2,7 +2,11 @@ import { bootstrap }    from '@angular/platform-browser-dynamic';
 
 import { AppComponent } from './app.component';
 import { appRouterProviders } from './app.routes';
+import {enableProdMode} from '@angular/core';
+import {provideForms, disableDeprecatedForms} from '@angular/forms';
+
+enableProdMode();
 
 bootstrap(AppComponent, [
-    appRouterProviders
+    appRouterProviders,disableDeprecatedForms(), provideForms()
 ]);
